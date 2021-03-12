d33afd734d6e0f3a68505a443e0603b9e4b1d4e3
kocedric <kocedric02@gmail.com>
2021-03-12 17:32:02 +0000

    site de gestion du tracker 1

--- main/admin.py
+++ main/admin.py
@@ -1,15 +1,15 @@
-from django.contrib import admin
-from .models import Portfolio
-
-
-class PortfolioAdmin(admin.ModelAdmin):
-    fields = [
-        (None, {'fields': ['title', 'image']}),
-        ('Description', {'fields': ['date', 'description']})
-    ]
-    list_display = ('title', 'date')
-    list_filter = ['title', 'date']
-    search_fields = ['title', 'date']
-
-
-admin.site.register(Portfolio, PortfolioAdmin)
+# from django.contrib import admin
+# from .models import Portfolio
+#
+#
+# class PortfolioAdmin(admin.ModelAdmin):
+#     fields = [
+#         (None, {'fields': ['title', 'image']}),
+#         ('Description', {'fields': ['date', 'description']})
+#     ]
+#     list_display = ('title', 'date')
+#     list_filter = ['title', 'date']
+#     search_fields = ['title', 'date']
+#
+#
+# admin.site.register(Portfolio, PortfolioAdmin)
--- tracker/settings.py
+++ tracker/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main'
 ]
 
 MIDDLEWARE = [
@@ -75,12 +76,8 @@ WSGI_APPLICATION = 'tracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'tracker',
-        'USER': 'cedric',
-        'PASSWORD': 'yvonne123',
-        'HOST': '',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
